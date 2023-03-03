6c396d2b7a3c9a07271c8368425c1a0ef7ae1978
user <ggmodhunters@gmail.com>
2023-03-03 10:57:33 +0200

    version 1.3.2

--- risovanie.py
+++ risovanie.py
@@ -1,9 +1,11 @@
 import pygame,model
-
+porgran=pygame.Rect(50,50,50,50)
 screen = pygame.display.set_mode([1500, 1000])
-
+pygame.draw.rect(screen,[255, 50,21],porgran)
 def hydozhnik():
     screen.fill([34, 56, 12])
     pygame.draw.rect(screen, [56, 98, 21],model.kk)
 def updater():
     pygame.display.flip()
+# def pogranichnik():
+    # if porgran.width ==
